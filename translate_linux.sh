8ba165ac7f2df8748d0f540d1dd30159027d03e6
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-05-27 18:04:07 +0200

    add dict_sq to translate_linux.sh

--- translate_linux.sh
+++ translate_linux.sh
@@ -58,6 +58,7 @@ translate pl dict_pl
 translate pt_BR dict_pt-BR
 translate pt_PT dict_pt-PT
 translate ru dict_ru
+translate sq dict_sq
 translate sv dict_sv
 translate tr dict_tr
 translate uk dict_uk
